ae9002d8e4d6cd631f4d4bfa47654524b72beffb
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-07-27 01:48:36 +0600

    Variabledeclaration.c

---
+++ Variabledeclaration.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+  //here we have instantiated two datatypes.
+  int no = 2;  // this is an integer data..
+  char name = 'Rayhan';   // this is a string as a character..
+
+  // Now we will viwe the output..
+  printf("Name: %c. \n No: %d", name, no);
+}
