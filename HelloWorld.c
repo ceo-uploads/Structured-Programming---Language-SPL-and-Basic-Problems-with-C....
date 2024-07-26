1c05685219511f1447d8bc4ab1a57559fd4177e3
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-07-26 23:01:52 +0600

    HelloWorld.c
    -(Base)-

---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+  //It is the basic of C,, it is used to show a simple text in the output..
+  printf("Hello World!");
+
+  // The output will be the text - "Hello World!" only.
+}
