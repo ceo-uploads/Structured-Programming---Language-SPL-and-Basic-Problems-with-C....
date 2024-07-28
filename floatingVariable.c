2e6a0b3ab6a1c2f0f68ae323be37c1c816b8c696
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-07-29 00:47:47 +0600

    floatingVariable.c

---
+++ floatingVariable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  float a , b, c;
+  //floating instances..
+  scanf("%f %2f %3f", &a, &b, &c);
+
+  /*there in the input section,, it will take floating 
+  point variable for a = 1point, b = 2point, c = 3point*/
+
+  printf("%f, %2f, %3f",a ,b, c);
+  //output....
+}
