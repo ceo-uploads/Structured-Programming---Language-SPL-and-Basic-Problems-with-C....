28d49ec8e24c3ab74aa5bce3feccd0316d18f53c
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-01 15:22:19 +0600

    controlStatement.c

---
+++ controlStatement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+  //Local variable defination..
+  int a = 10;
+
+  //checking boolean conditions using if statement..
+
+  if (a < 20) {
+    //if boolean statement is true,, than the output will be printed
+    printf("a is less than 20");
+  } else {
+    //if flase...
+    printf("a is not less than 20");
+  }
+
+  return 0;
+}
