71fcfa5b3603a3051bf83cb365a67c43ef4658ca
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-09 23:30:12 +0600

    For Loop in C
    Being a For loop and it's usage..

---
+++ forLoop.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main() {
+  printf("Numbers between 1 to 10:\n ");
+  for (int i = 1; i <= 10; i++) { // i can be instances, starting from 1 and will continue til 10 with the operator <=, and incremental operator ++
+    printf("%d\.n",i); // it will print the numbers as i;
+  }
+
+  return 0;
+}
